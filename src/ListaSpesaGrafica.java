9fcb024992509f985cbadab561ae9599d73ccd75
menegondiego <menegondiego@Lab06_07.itseinaudi.local>
2016-01-30 09:25:07 +0100

    <cc

--- src/ListaSpesaGrafica.java
+++ src/ListaSpesaGrafica.java
@@ -7,6 +7,10 @@ import menegondiego.ListaSpesa;
 import menegondiego.NonAlimentari;
 
 import org.eclipse.swt.widgets.Text;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Button;
@@ -15,6 +19,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.events.TouchListener;
 import org.eclipse.swt.events.TouchEvent;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class ListaSpesaGrafica {
 
@@ -40,6 +45,7 @@ public class ListaSpesaGrafica {
 	private Text tot;
 	int i;
 	int pr=0;
+	boolean c=false;
 	
 	
 	
@@ -60,6 +66,14 @@ public class ListaSpesaGrafica {
 	/**
 	 * Open the window.
 	 */
+	String className = "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
+	static{
+		try{
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(Exception e){}
+		}
+	
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
@@ -77,7 +91,7 @@ public class ListaSpesaGrafica {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(702, 300);
+		shell.setSize(702, 332);
 		shell.setText("SWT Application");
 		
 		cod = new Text(shell, SWT.BORDER);
@@ -107,6 +121,7 @@ public class ListaSpesaGrafica {
 			public void widgetSelected(SelectionEvent e) {
 				 nalimentare=false;
 				 alimentare = true;
+				 c=true;
 			}
 		});
 		btnAlimentare.setBounds(10, 87, 90, 16);
@@ -118,6 +133,7 @@ public class ListaSpesaGrafica {
 			public void widgetSelected(SelectionEvent e) {
 				 nalimentare=true;
 				 alimentare = false;
+				 c=true;
 			}
 		});
 		btnNonAlimentare.setBounds(10, 116, 100, 16);
@@ -144,45 +160,55 @@ public class ListaSpesaGrafica {
 				n=list.getSelectionIndex();
 			}
 		});
-		list.setBounds(405, 10, 271, 242);
+		list.setBounds(405, 10, 271, 273);
 		
 		Button btnAggiungiProdotto = new Button(shell, SWT.NONE);
 		btnAggiungiProdotto.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				if(alimentare==true){
+				if(alimentare==true&&c==true){
 					
 					System.out.println("Alimentare");
 					System.out.println(tf);
-					cb=cod.getText();
-					desc=pro.getText();
-					prezzo=Double.valueOf(pre.getText());
-					Data d=new Data(Integer.valueOf(g.getText()),Integer.valueOf(m.getText()),Integer.valueOf(a.getText()));
-					p=new Alimentari(cb,desc,prezzo,d);
+				
+					
+					
 					try {
+						cb=cod.getText();
+						desc=pro.getText();
+						prezzo=Double.valueOf(pre.getText());
+						Data d=new Data(Integer.valueOf(g.getText()),Integer.valueOf(m.getText()),Integer.valueOf(a.getText()));
+						p=new Alimentari(cb,desc,prezzo,d);
 						ls.AggiungiProdotto(p,tf);
 						list.add(p.toString());
 						pr++;
 					} catch (Exception e1) {
 						// TODO Auto-generated catch block
+						JOptionPane.showMessageDialog(null,"Dati non validi","Errore",JOptionPane.ERROR_MESSAGE);
 						e1.printStackTrace();
 					}
 				}else{
-					System.out.println("Nonalimentare");
-					System.out.println(tf);
-					cb=cod.getText();
-					desc=pro.getText();
-					prezzo=Double.valueOf(pre.getText());
-					ma=mat.getText();
-					p1=new NonAlimentari(cb,desc,prezzo,ma);
-					try {
-						ls.AggiungiProdotto(p1,tf);
-						list.add(p1.toString());
-						pr++;
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+					if(c==true){
+						System.out.println("Nonalimentare");
+						System.out.println(tf);
+						
+						
+						try {
+							cb=cod.getText();
+							desc=pro.getText();
+							prezzo=Double.valueOf(pre.getText());
+							ma=mat.getText();
+							p1=new NonAlimentari(cb,desc,prezzo,ma);
+							ls.AggiungiProdotto(p1,tf);
+							list.add(p1.toString());
+							pr++;
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							JOptionPane.showMessageDialog(null,"Dati non validi","Errore",JOptionPane.ERROR_MESSAGE);
+							e1.printStackTrace();
+						}
 					}
+					
 				}
 				
 			}
@@ -225,14 +251,14 @@ public class ListaSpesaGrafica {
 				tot.setText(""+ls.CalcolaSpesa());
 			}
 		});
-		btnCalcolaTotale.setBounds(166, 195, 85, 25);
+		btnCalcolaTotale.setBounds(10, 258, 118, 25);
 		btnCalcolaTotale.setText("Calcola totale");
 		
 		tot = new Text(shell, SWT.BORDER);
-		tot.setBounds(310, 195, 76, 21);
+		tot.setBounds(310, 260, 76, 21);
 		
 		Label lblTot = new Label(shell, SWT.NONE);
-		lblTot.setBounds(275, 200, 29, 15);
+		lblTot.setBounds(275, 263, 29, 15);
 		lblTot.setText("TOT");
 		
 		Button btnEliminaProdotto = new Button(shell, SWT.NONE);
@@ -243,7 +269,7 @@ public class ListaSpesaGrafica {
 					ls.eliminaProdotto(n);
 					list.remove(n);
 				}catch(Exception c){
-					
+					JOptionPane.showMessageDialog(null,"Lista vuota","Errore",JOptionPane.ERROR_MESSAGE);
 				}
 				
 			}
@@ -265,16 +291,23 @@ public class ListaSpesaGrafica {
 		svuota.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				for(i=0;i<list.getSelectionIndex();i++){
-					ls.eliminaProdotto(i);
-					
-				}
 				list.removeAll();
-				
+				ls=new ListaSpesa(tf);
+				tot.setText(""+ls.CalcolaSpesa());
 			}
 		});
-		svuota.setBounds(166, 164, 85, 25);
+		svuota.setBounds(166, 258, 85, 25);
 		svuota.setText("Svuota carrello");
+		
+		Button btnCaricaCarrello = new Button(shell, SWT.NONE);
+		btnCaricaCarrello.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				ls.carica(list,shell);
+			}
+		});
+		btnCaricaCarrello.setBounds(166, 195, 85, 25);
+		btnCaricaCarrello.setText("Carica carrello");
 		
 		
 
